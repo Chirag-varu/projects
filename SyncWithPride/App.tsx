8ebb0cedcd18e45685951f2a00cebc9481b4e522
Chirag varu <133989137+Chirag-varu@users.noreply.github.com>
2024-02-28 18:58:15 +0530

    Add files via upload

---
+++ SyncWithPride/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { Login } from "./Component/FirstComponent/login";
+import { Profile } from "./Component/SecondComponent/Profile";
+import { Aboutus } from "./Component/ThirdComponent/Aboutus";
+import { Booking } from "./Component/FourthComponent/Booking";
+import { Gateway } from "./Component/FivethComponent/Gateway";
+import { Mainpage } from "./Component/EightComponent/Mainpage";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        {/* <Login /> */}
+        {/* <Profile /> // Log out modal*/}
+        {/* <Aboutus /> */}
+        {/* <Booking /> */}
+        {/* <Gateway /> // Booking Modal  */}
+        {/* <Mainpage /> */}
+      </>
+    </div>
+  );
+}
+
+export default App;
